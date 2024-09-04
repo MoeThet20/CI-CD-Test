a36ada0676cfd45015a9a25edb8faea177cade2f
MoeThet20 <moethet.nice@gmail.com>
2024-09-04 16:51:00 +0700

    [ modify ] change text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Hello CI/CD" });
+  res.render("index", { title: "Successfully Oki" });
 });
 
 module.exports = router;
